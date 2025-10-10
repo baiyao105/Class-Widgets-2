f859a6d43b32803c40122a0a3351049f2efdafcb
RinLit <lintusup@foxmail.com>
2025-10-11 03:05:11 +0800

    fix(core): 在macOS运行时，ClassWidgets会无限创建进程的恶性问题

--- src/app.py
+++ src/app.py
@@ -2,13 +2,13 @@ from PySide6.QtWidgets import QApplication
 from src.core import AppCentral
 import sys
 
+from src.core.utils import ensure_single_instance
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
+    ensure_single_instance()
 
+    app = QApplication(sys.argv)
     instance = AppCentral()
     instance.run()
-    # debugger_central = DebuggerCentral(instance)
-    # debugger = DebuggerWindow(instance)
 
     app.exec()
