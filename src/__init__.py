baaa6e76c2e8c942053a6eda80620cf0197d9aa9
RinLit <lintusup@foxmail.com>
2025-10-22 03:00:28 +0800

    feat(课程表编辑器): 支持导入、导出 CSES 课程表文件 #31

--- src/__init__.py
+++ src/__init__.py
@@ -5,3 +5,6 @@ __version__ = "2.0.0.dev20251018"
 __version_type__ = "alpha"
 __app_name__ = "ClassWidgets_2"
 __app_filename__ = sys.argv[0]
+
+__SCHEDULE_SCHEMA_VERSION__ = 1
+__CSES_SCHEMA_VERSION__ = 1
